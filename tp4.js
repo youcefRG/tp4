d9c15c9df6e1167a8210003b7aebc1f9b7a5c97b
youcef kolli <32937714+youcefRG@users.noreply.github.com>
2018-08-20 12:03:48 +0100

    Add files via upload

---
+++ tp4.js
@@ -0,0 +1,37 @@
+function p1() {
+    document.getElementById("big_pict").src = "photo1.png";
+    document.getElementById("titre").innerHTML = "Titre de la photo 1";
+
+}
+function p2() {
+    document.getElementById("big_pict").src="photo2.png";
+    document.getElementById("titre").innerHTML ="Titre de la photo 2";
+}
+ function p3() {
+    document.getElementById("big_pict").src = "photo3.png";
+     document.getElementById("titre").innerHTML = "Titre de la photo 3";
+}
+function p4() {
+    document.getElementById("big_pict").src = "photo4.png";
+    document.getElementById("titre").innerHTML = "Titre de la photo 4";
+}
+function p5() {
+    document.getElementById("big_pict").src = "photo5.png";
+    document.getElementById("titre").innerHTML = "Titre de la photo 5 ";
+}
+function p6() {
+    document.getElementById("big_pict").src = "photo6.png";
+    document.getElementById("titre").innerHTML = "Titre de la photo 6";
+}
+function p7() {
+    document.getElementById("big_pict").src = "photo7.png";
+    document.getElementById("titre").innerHTML = "Titre de la photo 7";
+}
+function p8() {
+    document.getElementById("big_pict").src = "photo8.png";
+    document.getElementById("titre").innerHTML = "Titre de la photo 8";
+}
+function p9() {
+    document.getElementById("big_pict").src = "photo9.png";
+    document.getElementById("titre").innerHTML = "Titre de la photo 9";
+}
